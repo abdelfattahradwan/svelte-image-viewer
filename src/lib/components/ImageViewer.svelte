dbda18f1046440f7875b82617a1a18917b75da7c
Abdelfattah-Radwan <ARadwan620@gmail.com>
2024-05-01 10:18:35 +0300

    breaking: Update ImageViewer component with panning and zooming options
    - Added new panning and zooming options properties to allow for the customisation of panning/zooming tweening options.
    - Replace classes with inline styles.
    - Remove unnecessary div.

--- src/lib/components/ImageViewer.svelte
+++ src/lib/components/ImageViewer.svelte
@@ -2,27 +2,32 @@
   import panToMove from "$lib/utilities/gestures/pan-to-move.js";
   import mouseWheelZoom from "$lib/utilities/gestures/mouse-wheel-zoom.js";
   import pinchToZoom from "$lib/utilities/gestures/pinch-to-zoom.js";
-  import { type Tweened, tweened } from "svelte/motion";
+  import { type TweenedOptions, type Tweened, tweened } from "svelte/motion";
 
   export let src: string;
-  export let minZoom: number = 0.5;
-  export let maxZoom: number = 2.0;
+
+  export let panningOptions: TweenedOptions<number> = {
+    duration: 125,
+  };
+
+  export let zoomingOptions: {
+    minZoom: number;
+    maxZoom: number;
+    smoothing: number;
+  } & TweenedOptions<number> = {
+    minZoom: 0.5,
+    maxZoom: 2.0,
+    smoothing: 256,
+    duration: 125,
+  };
 
   let targetOffsetX: number = 0;
   let targetOffsetY: number = 0;
   let targetZoom: number = 1.0;
 
-  const offsetX: Tweened<number> = tweened(0.0, {
-    duration: 125,
-  });
-
-  const offsetY: Tweened<number> = tweened(0.0, {
-    duration: 125,
-  });
-
-  const zoom: Tweened<number> = tweened(1.0, {
-    duration: 125,
-  });
+  const offsetX: Tweened<number> = tweened(0.0, panningOptions);
+  const offsetY: Tweened<number> = tweened(0.0, panningOptions);
+  const zoom: Tweened<number> = tweened(1.0, zoomingOptions);
 
   $: {
     offsetX.set(targetOffsetX);
@@ -30,47 +35,31 @@
     zoom.set(targetZoom);
   }
 
+  function setTargetOffset(deltaX: number, deltaY: number) {
+    targetOffsetX += deltaX;
+    targetOffsetY += deltaY;
+  }
+
   function setTargetZoom(delta: number) {
     targetZoom = Math.max(
-      minZoom,
-      Math.min(maxZoom, targetZoom * (1.0 - delta / 250.0))
+      zoomingOptions.minZoom,
+      Math.min(
+        zoomingOptions.maxZoom,
+        targetZoom * (1.0 - delta / zoomingOptions.smoothing)
+      )
     );
   }
 </script>
 
-<div class="image-viewer">
-  <div
-    use:panToMove={(deltaX, deltaY) => {
-      targetOffsetX += deltaX;
-      targetOffsetY += deltaY;
-    }}
-    use:mouseWheelZoom={setTargetZoom}
-    use:pinchToZoom={setTargetZoom}
-    class="image-viewer-viewport"
-  >
-    <img
-      {src}
-      alt=""
-      style="transform: translate({$offsetX}px, {$offsetY}px) scale({$zoom})"
-      class="image-viewer-image"
-    />
-  </div>
+<div
+  style="position: absolute; top: 0; left: 0; right: 0; bottom: 0; overflow: hidden;"
+  use:panToMove={setTargetOffset}
+  use:mouseWheelZoom={setTargetZoom}
+  use:pinchToZoom={setTargetZoom}
+>
+  <img
+    {src}
+    alt=""
+    style="display: block; pointer-events: none; transform: translate({$offsetX}px, {$offsetY}px) scale({$zoom})"
+  />
 </div>
-
-<style>
-  .image-viewer {
-    position: absolute;
-    inset: 0;
-  }
-
-  .image-viewer-viewport {
-    position: absolute;
-    inset: 0;
-    overflow: hidden;
-  }
-
-  .image-viewer-image {
-    display: block;
-    pointer-events: none;
-  }
-</style>
