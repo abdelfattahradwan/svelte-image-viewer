bc0827fd3377afa6587d010e83e6d3161dfa0ed9
Abdelfattah-Radwan <ARadwan620@gmail.com>
2024-05-01 12:25:30 +0300

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
   import "../app.css";
   import ImageViewer from "$lib/components/ImageViewer.svelte";
+  import Viewer from "$lib/components/Viewer.svelte";
+  import { cubicOut } from "svelte/easing";
 </script>
 
 <h1>Welcome to Svelte Image Viewer!</h1>
@@ -15,7 +17,34 @@
 
 <div
   id="image-viewer-container"
-  style="position: relative; height: 512px; user-select: none;"
+  style="position: relative; height: 512px; user-select: none; border: 1px solid white;"
 >
-  <ImageViewer src="https://source.unsplash.com/random/800x600" />
+  <ImageViewer src="https://source.unsplash.com/random/768x512" />
+</div>
+
+<div
+  id="viewer-1-container"
+  style="position: relative; height: 512px; user-select: none; border: 1px solid white;"
+>
+  <Viewer
+    offsetTweenOptions={{
+      duration: 250,
+      easing: cubicOut,
+    }}
+    scaleTweenOptions={{
+      duration: 250,
+      easing: cubicOut,
+    }}
+  >
+    <img src="https://source.unsplash.com/random/768x512" alt="" />
+  </Viewer>
+</div>
+
+<div
+  id="viewer-2-container"
+  style="position: relative; height: 512px; user-select: none; border: 1px solid white;"
+>
+  <Viewer>
+    <p>This is a paragraph inside the viewer</p>
+  </Viewer>
 </div>
