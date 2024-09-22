3ed7d65374b0664e015a62eccf280be087a73dac
Abdelfattah-Radwan <ARadwan620@gmail.com>
2024-09-22 23:12:33 +0300

    feat: add examples for the new experimental components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,8 @@
   import ImageViewer from "$lib/components/ImageViewer.svelte";
   import Viewer from "$lib/components/Viewer.svelte";
   import { cubicOut } from "svelte/easing";
+  import ExperimentalImageViewer from "$lib/components/experimental/ImageViewer.svelte";
+  import ExperimentalViewer from "$lib/components/experimental/Viewer.svelte";
 </script>
 
 <h1>Welcome to Svelte Image Viewer!</h1>
@@ -14,6 +16,33 @@
     >the GitHub repository</a
   > to read the documentation
 </p>
+
+<div
+  id="image-viewer-container"
+  style="position: relative; height: 512px; user-select: none; border: 1px solid white;"
+>
+  <ExperimentalImageViewer
+    src="https://picsum.photos/256"
+    minScale={0.5}
+    maxScale={10.0}
+  />
+</div>
+
+<div
+  id="viewer-1-container"
+  style="position: relative; height: 512px; user-select: none; border: 1px solid white;"
+>
+  <ExperimentalViewer >
+    <form>
+      <label for="scale">Scale</label>
+      <input type="range" id="scale" min="0.5" max="10" step="0.1" />
+      <label for="offsetX">Offset X</label>
+      <input type="range" id="offsetX" min="0" max="100" step="1" />
+      <label for="offsetY">Offset Y</label>
+      <input type="range" id="offsetY" min="0" max="100" step="1" />
+    </form>
+  </ExperimentalViewer>
+</div>
 
 <div
   id="image-viewer-container"
