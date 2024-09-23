45fd2f8f92a54bc889d018a101df447fb95609b9
Abdelfattah-Radwan <ARadwan620@gmail.com>
2024-09-23 14:38:13 +0300

    feat: consolidate zoom/pan functionality in ImageViewer and Viewer components; replace deprecated options with unified tweenOptions
    Unifying the tween options prevents jitter and sometimes incorrect transformations when tween durations are not the same.
    
    Moving to the `panAndZoom` action instead of multiple separate actions prevents actions fighting each other over the control of the stores. Also proper pinch-to-zoom and mouse wheel zoom was not feasible with separate actions.

--- src/lib/components/ImageViewer.svelte
+++ src/lib/components/ImageViewer.svelte
@@ -1,54 +1,53 @@
 <script lang="ts">
-  import panToMove from "$lib/utilities/gestures/pan-to-move.js";
-  import mouseWheelZoom from "$lib/utilities/gestures/mouse-wheel-zoom.js";
-  import pinchToZoom from "$lib/utilities/gestures/pinch-to-zoom.js";
-  import { type TweenedOptions, type Tweened, tweened } from "svelte/motion";
+  import panAndZoom from "$lib/actions/pan-and-zoom.js";
+  import { type TweenedOptions, tweened } from "svelte/motion";
+  import { cubicOut } from "svelte/easing";
 
   export let src: string;
-  export let minScale: number = 0.5;
-  export let maxScale: number = 2.0;
-  export let scaleSmoothing: number = 250;
-
-  export let offsetTweenOptions: TweenedOptions<number> = {
-    duration: 125,
-  };
-
-  export let scaleTweenOptions: TweenedOptions<number> = {
-    duration: 125,
-  };
-
   export let targetOffsetX: number = 0;
   export let targetOffsetY: number = 0;
   export let targetScale: number = 1.0;
+  export let minScale: number = 0.5;
+  export let maxScale: number = 3.0;
+  export let scaleSmoothing: number = 500;
 
-  const offsetX: Tweened<number> = tweened(0.0, offsetTweenOptions);
-  const offsetY: Tweened<number> = tweened(0.0, offsetTweenOptions);
-  const scale: Tweened<number> = tweened(1.0, scaleTweenOptions);
+  export let tweenOptions: TweenedOptions<number> = {
+    duration: 300,
+    easing: cubicOut,
+  };
+
+  /**
+   * @deprecated use `tweenOptions` instead
+   */
+  export let offsetTweenOptions: TweenedOptions<number> = {};
+
+  /**
+   * @deprecated use `tweenOptions` instead
+   */
+  export let scaleTweenOptions: TweenedOptions<number> = {};
+
+  const offsetX = tweened(0.0, tweenOptions);
+  const offsetY = tweened(0.0, tweenOptions);
+  const scale = tweened(1.0, tweenOptions);
 
   $: {
     offsetX.set(targetOffsetX);
     offsetY.set(targetOffsetY);
-    scale.set(targetScale);
   }
 
-  function setTargetOffset(deltaX: number, deltaY: number) {
-    targetOffsetX += deltaX;
-    targetOffsetY += deltaY;
-  }
-
-  function setTargetScale(delta: number) {
-    targetScale = Math.max(
-      minScale,
-      Math.min(maxScale, targetScale * (1.0 - delta / scaleSmoothing)),
-    );
-  }
+  $: scale.set(targetScale);
 </script>
 
 <div
+  use:panAndZoom={{
+    offsetX,
+    offsetY,
+    scale,
+    minScale,
+    maxScale,
+    scaleSmoothing,
+  }}
   style="display: flex; position: absolute; align-items: center; justify-content: center; top: 0; left: 0; right: 0; bottom: 0; overflow: hidden; touch-action: none;"
-  use:panToMove={setTargetOffset}
-  use:mouseWheelZoom={setTargetScale}
-  use:pinchToZoom={setTargetScale}
 >
   <img
     {src}
