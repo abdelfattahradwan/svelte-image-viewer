47f4da99dea04a00d403ddc84b990c75917c826b
abdelfattahradwan <ARadwan620@gmail.com>
2024-09-28 13:05:25 +0300

    style: wrap content in a <main> tag in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,28 +4,28 @@
   import Viewer from "$lib/components/Viewer.svelte";
 </script>
 
-<h1>Welcome to Svelte Image Viewer!</h1>
-<p>
-  This is a simple image viewer component that supports panning and zooming.
-</p>
-<p>
-  Visit <a href="https://github.com/abdelfattahradwan/svelte-image-viewer"
-    >the GitHub repository</a
-  > to read the documentation
-</p>
-
-<div
-  id="image-viewer-container"
-  style="position: relative; height: 512px; user-select: none; border: 1px solid white;"
->
-  <ImageViewer src="https://picsum.photos/256" />
-</div>
-
-<div
-  id="viewer-1-container"
-  style="position: relative; height: 512px; user-select: none; border: 1px solid white;"
->
-  <Viewer>
-    <img src="https://picsum.photos/256" alt="" />
-  </Viewer>
-</div>
+<main>
+  <h1>Welcome to Svelte Image Viewer!</h1>
+  <p>
+    This is a simple image viewer component that supports panning and zooming.
+  </p>
+  <p>
+    Visit <a href="https://github.com/abdelfattahradwan/svelte-image-viewer"
+      >the GitHub repository</a
+    > to read the documentation
+  </p>
+  <div
+    id="image-viewer-container"
+    style="position: relative; height: 512px; user-select: none; border: 1px solid white;"
+  >
+    <ImageViewer src="https://picsum.photos/256" />
+  </div>
+  <div
+    id="viewer-1-container"
+    style="position: relative; height: 512px; user-select: none; border: 1px solid white;"
+  >
+    <Viewer>
+      <img src="https://picsum.photos/256" alt="" />
+    </Viewer>
+  </div>
+</main>
