eed342818205712ac0212723798dfd8d18fd90fb
abdelfattahradwan <aradwan620@gmail.com>
2025-10-11 21:24:00 +0300

    fix: remove unused scaleSmoothing property from Viewer component

--- src/lib/components/Viewer.svelte
+++ src/lib/components/Viewer.svelte
@@ -60,7 +60,6 @@
     },
     minScale,
     maxScale,
-    scaleSmoothing: smoothing,
   })}
   style="display: flex; position: absolute; align-items: center; justify-content: center; top: 0; left: 0; right: 0; bottom: 0; overflow: hidden; touch-action: none;"
 >
