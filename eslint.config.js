bf539c1616dfcad2a1672fcd265cae1a8c2bdaa0
abdelfattahradwan <aradwan620@gmail.com>
2026-07-08 23:06:47 +0300

    chore: add ESLint

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+import prettier from "eslint-config-prettier";
+import path from "node:path";
+import js from "@eslint/js";
+import svelte from "eslint-plugin-svelte";
+import { defineConfig, includeIgnoreFile } from "eslint/config";
+import globals from "globals";
+import ts from "typescript-eslint";
+
+const gitignorePath = path.resolve(import.meta.dirname, ".gitignore");
+
+export default defineConfig(
+  includeIgnoreFile(gitignorePath),
+  js.configs.recommended,
+  ts.configs.recommended,
+  svelte.configs.recommended,
+  prettier,
+  svelte.configs.prettier,
+  {
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    rules: {
+      "no-undef": "off",
+    },
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    extends: [...ts.configs.strictTypeChecked],
+    languageOptions: {
+      parser: ts.parser,
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
+  {
+    files: ["**/*.svelte", "**/*.svelte.ts", "**/*.svelte.js"],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        extraFileExtensions: [".svelte"],
+        parser: ts.parser,
+      },
+    },
+  },
+  {
+    rules: {
+      "svelte/prefer-svelte-reactivity": "off",
+    },
+  },
+);
